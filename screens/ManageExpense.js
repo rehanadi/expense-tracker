ed56ba884b0caca9fe35300c46a5c7ceb4dc5678
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-03-18 15:15:13 +0700

    first commit

---
+++ screens/ManageExpense.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+const ManageExpense = () => {
+  return (
+    <Text>ManageExpense</Text>
+  );
+};
+
+export default ManageExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+const RecentExpenses = () => {
+  return (
+    <Text>RecentExpenses</Text>
+  );
+};
+
+export default RecentExpenses;
